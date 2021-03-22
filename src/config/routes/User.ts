a2268f043dd95dd7874e199c1941277aeba1935a
David Calle Daza <dcalled1@eafit.edu.co>
2021-03-22 17:06:27 -0500

    Controllers

--- src/config/routes/User.ts
+++ src/config/routes/User.ts
@@ -1,40 +1,41 @@
 import CommonRoutesConfig from './Common';
-import {Application, Request, Response, NextFunction} from 'express';
+import { Application, Request, Response, NextFunction } from 'express';
+import UserController from '../../controllers/UserController';
 
 export default class UsersRoutes extends CommonRoutesConfig {
+    controller: UserController;
+
     constructor(app: Application) {
         super(app, 'UsersRoutes');
+        this.controller = new UserController();
     }
 
     configureRoutes() {
         this.app.route(`/users`)
-        .get((req: Request, res: Response) => {
-            res.status(200).send(`List of users`);
-        })
-        .post((req: Request, res: Response) => {
-            res.status(200).send(`Post to users`);
-        });
+            .get((req: Request, res: Response) => {
+                this.controller.index(req, res);
+            });
 
-    this.app.route(`/users/:userId`)
-        .all((req: Request, res: Response, next: NextFunction) => {
-            // this middleware function runs before any request to /users/:userId
-            // but it doesn't accomplish anything just yet---
-            // it simply passes control to the next applicable function below using next()
-            next();
-        })
-        .get((req: Request, res: Response) => {
-            res.status(200).send(`GET requested for id ${req.params.userId}`);
-        })
-        .put((req: Request, res: Response) => {
-            res.status(200).send(`PUT requested for id ${req.params.userId}`);
-        })
-        .patch((req: Request, res: Response) => {
-            res.status(200).send(`PATCH requested for id ${req.params.userId}`);
-        })
-        .delete((req: Request, res: Response) => {
-            res.status(200).send(`DELETE requested for id ${req.params.userId}`);
-        });
+        this.app.route(`/users/new`)
+            .all((req: Request, res: Response, next: NextFunction) => {
+                this.controller.validateUser(req, res);
+                next();
+            })
+            .post((req: Request, res: Response) => {
+                this.controller.registerUser(req, res);
+            });
+
+        this.app.route(`/users/key/new`)
+            .all((req: Request, res: Response, next: NextFunction) => {
+                this.controller.validateUser(req, res);
+                next();
+            })
+            .post((req: Request, res: Response) => {
+                this.controller.registerKey(req, res);
+            });
+
         
+
         return this.app;
     }
 }
---
+++ src/controllers/UserController.ts
@@ -0,0 +1,30 @@
+import BaseController from "./Common";
+import {Request, Response,} from 'express';
+
+
+export default class UserController implements BaseController {
+    constructor() {
+
+    }
+
+    index(req: Request, res: Response) {
+        
+    }
+
+    registerUser(req: Request, res: Response) {
+        
+    }
+
+    validateUser(req: Request, res: Response) {
+        
+    }
+
+    registerKey(req: Request, res: Response) {
+        
+    }
+
+    validateKey(req: Request, res: Response) {
+        
+    }
+
+}
